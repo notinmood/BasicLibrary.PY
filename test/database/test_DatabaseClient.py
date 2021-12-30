9e7358962ade1f1d0a62a6a833f3c09f5bc7d82a
解大劦 <9727005@qq.com>
2021-12-30 09:53:39 +0800

    feature: 添加实体数据的比较功能

--- test/database/test_DatabaseClient.py
+++ test/database/test_DatabaseClient.py
@@ -6,15 +6,15 @@
  * @creator: ShanDong Xiedali
  * @company: HiLand & RainyTop
 """
-from hilandBasicLibrary.dataBase.databaseClient import DatabaseClient
-from hilandBasicLibrary.data.stringHelper import StringHelper
+import hilandBasicLibrary
 from hilandBasicLibrary.configHelper import ConfigHelper
+from hilandBasicLibrary.dataBase.databaseClient import DatabaseClient
 
 
 def test_get_mate():
     mate = DatabaseClient.get_mate("hello")
-    actual = mate.__str__()
-    expected = "mate in  DatabaseMate: hilandBasicLibrary.dataBase.MySql.mate"
+    actual = type(mate)
+    expected = hilandBasicLibrary.dataBase.MySql.mate.Mate
     assert actual == expected
 
     actual = mate.get_real_table_name()
@@ -32,21 +32,3 @@ def test_get_ddl():
 
     ddl = DatabaseClient.get_ddl()
     print(ddl)
-
-
-# def test_ddl_create_table():
-#     mate = DatabaseClient.get_mate("my_user")
-#     sql = """CREATE TABLE `dp_ss_my_user__test` (
-#   `id` int(11) NOT NULL AUTO_INCREMENT,
-#   `name` varchar(255) CHARACTER SET utf8 DEFAULT '',
-#   `email` varchar(255) CHARACTER SET utf8 DEFAULT '',
-#   `birthday` datetime DEFAULT NULL,
-#   PRIMARY KEY (`id`)
-# ) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=latin1"""
-#     mate.directly_exec(sql)
-
-
-# def test_s():
-#     sql = 'INSERT INTO `tmp_user__dupl` (`id`,`name`,`birthday`,`postcode`) VALUES (1,"zhangsan","2021-12-24 09:07:05",266000),(4,"lisi","2021-12-15 09:07:26",277521);'
-#     mate = DatabaseClient.get_mate("user")
-#     mate.directly_exec(sql)
