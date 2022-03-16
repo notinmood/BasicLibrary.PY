32aed7476f580dc999f30e1ddee409059c8469d2
解大劦 <9727005@qq.com>
2022-03-16 10:47:11 +0800

    feature: 添加Excel的copy/paste功能

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ URL = 'https://github.com/notinmood/BasicLibrary.PY'
 EMAIL = '9727005@qq.com'
 AUTHOR = 'xiedali'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '0.4.15'
+VERSION = '0.4.16'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
