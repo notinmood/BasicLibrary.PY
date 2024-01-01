1c938bd95c0c7d78e30d8379eb03a55db6980aa0
山东解大劦 <notinmood@gmail.com>
2024-01-01 09:26:13 +0800

    feat: 为影子文件，添加不销毁功能

--- BasicLibrary/io/fileShadowUsing.py
+++ BasicLibrary/io/fileShadowUsing.py
@@ -14,7 +14,7 @@ from BasicLibrary.io.pathHelper import PathHelper
 
 class FileShadowUsing:
     """
-    在不影响原始文件的情形下，对文件的影子进行操作，操作完成后销毁影子。
+    在不影响原始文件的情形下，对文件的影子进行操作，操作完成后默认销毁影子文件（可以在初始化的时候，设置保留影子文件）。
     在 with *** as ***： 语句内使用，as 后返回的是影子文件的文件全名称。
     """
 
@@ -24,9 +24,14 @@ class FileShadowUsing:
 
     pass
 
-    def __init__(self, file_full_name):
+    def __init__(self, file_full_name, is_auto_destroy=True):
+        """
+        通过给定的原始文件，生成一个影子文件。
+        :param file_full_name: 原始文件的文件全名称。
+        :param is_auto_destroy: 是否自动销毁影子文件。
+        """
         self._file_shadow_full_name = self.__generate_file_shadow(file_full_name)
-
+        self._is_auto_destroy = is_auto_destroy
     pass
 
     def __enter__(self):
@@ -35,8 +40,9 @@ class FileShadowUsing:
     pass
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        FileHelper.remove(self._file_shadow_full_name)
-
+        if self._is_auto_destroy:
+            FileHelper.remove(self._file_shadow_full_name)
+        pass
     pass
 
     @staticmethod
