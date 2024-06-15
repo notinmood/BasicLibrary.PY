a62c1a9709bd8bc658b48bb37b39e5504452a011
解大劦 <develope@163.com>
2024-06-15 11:00:26 +0800

    feat: 添加UUID的生成功能

---
+++ BasicLibrary/data/uuidHelper.py
@@ -0,0 +1,25 @@
+"""
+ * @file   : uuidHelper.py
+ * @time   : 上午10:52
+ * @date   : 2024/6/15
+ * @mail   : 9727005@qq.com
+ * @creator: ShanDong Xiedali
+ * @company: HiLand & RainyTop
+"""
+import ulid
+
+
+class UUIDHelper:
+    """
+    使用ulid生成UUID。
+    优势：
+    1. 可以保证UUID的唯一性。
+    2. 可以保证UUID的顺序性。
+    3. 可以保证UUID的随机性。
+    """
+    @staticmethod
+    def new():
+        return ulid.new()
+
+
+pass
